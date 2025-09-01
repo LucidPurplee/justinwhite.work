c0c29ddb6b51aadb3484ece2b5f347087ac5e025
Lucid <155584646+LucidPurplee@users.noreply.github.com>
2025-09-01 02:18:36 -0400

    first version 🎉

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import daisyui from "daisyui"
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Space Grotesk", "ui-sans-serif", "system-ui", "sans-serif"],
+      mono: ["Space Mono", "ui-monospace", "monospace"],
+    },
+  },
+  plugins: [daisyui],
+}
